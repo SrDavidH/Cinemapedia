d8d625fdd2ba3ef02995c1dff66161b0a3630ada
DHZ <david.henao@lotrading.com>
2024-03-06 14:46:56 -0500

    Primer commit

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+ static const name = 'home-screen';//nombre de la ruta
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:  Placeholder(),
+    );
+  }
+}
